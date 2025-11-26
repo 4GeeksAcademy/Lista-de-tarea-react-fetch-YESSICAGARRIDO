cc58cbf4945850f182be1ab1cf28efd164178bce
yessigarridowork-blip <yessigarrido.work@gmail.com>
2025-11-26 23:35:20 +0000

    lista de tarea con fetch

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,23 +1,96 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
+
 
 function TodoList(
 ) {
+
     const [todo, setTodo] = useState([
     ]);
     const [tarea, setTarea] = useState("");
+
+    useEffect(() => {
+        fetch('https://playground.4geeks.com/todo/users/YessicaGarrido')
+
+            .then(resp =>
+                resp.json())
+
+            .then(data => setTodo(data.todos))
+
+
+            .catch(error => {
+                console.log(error);
+            });
+
+    }, [])
 
 
 
     const handleKeyDown = (e) => {
         if (e.key === 'Enter' && tarea.trim() !== "") {
-            setTodo([...todo, tarea.trim()]);
-            setTarea("");
+            fetch('https://playground.4geeks.com/todo/todos/YessicaGarrido', {
+                method: "POST",
+                body: JSON.stringify({
+                    label: tarea,
+                    is_done: false
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(resp =>
+                    resp.json()
+                )
+                .then(data => {
+                    console.log(data);
+                    setTodo([...todo,
+
+                    {
+                        id: todo.length + 1,
+                        label: tarea,
+                        is_done: false
+
+                    }]);
+                    setTarea("");
+                })
+                .catch(error => {
+
+                    console.log(error);
+                })
+
         }
     }
-    function eliminarTarea(index) {
-        setTodo(todo.filter((tarea, i) => i !== index));
-        console.log(index);
+    function eliminarTarea(id) {
+        fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(resp => {
+                console.log(resp.ok);
+                console.log(resp.status);
+                if (resp.ok) {
+                    const tarea = todo.filter((tarea) => tarea.id !== id);
+                    console.log(id);
+                    setTodo((tarea));
+                }
+
+                return resp.json();
+            })
+            .then(data => {
+
+                console.log(data);
+            })
+            .catch(error => {
+
+                console.log(error);
+            });
+
+        //setTodo(todo.filter((tarea, i) => i !== index)); console.log(index);
+
+
 
     }
 
@@ -39,12 +112,10 @@ function TodoList(
 
             />
             <ul>
-                {todo.map((tarea, index) => (
-                    <li key={index} className="tarea-item">
-
-
-                        <span>{tarea}</span>
-                        <button onClick={() => eliminarTarea(index)} className="clean">X</button>
+                {todo.map((tarea) => (
+                    <li key={tarea.id} className="tarea-item">
+                        <span>{tarea.label}</span>
+                        <button onClick={() => eliminarTarea(tarea.id)} className="clean">X</button>
 
                     </li>
                 ))}
